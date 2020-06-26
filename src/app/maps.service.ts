8d5b182901749c95b3c5f6b12eeb4e4cefbc3e99
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2020-06-26 06:30:54 -0500

    Preparando peticion a webservic

--- src/app/maps.service.ts
+++ src/app/maps.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
 
 interface Location {
   latitude: string;
@@ -16,6 +16,20 @@ export class MapsService {
   constructor(private http: HttpClient) {}
 
   getLocation() {
-    return this.http.get<Location>("https://ipapi.co/json/");
+    //return this.http.get<Location>("https://ipapi.co/json/");
+
+
+    const myheader = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    let body = new HttpParams();
+    body = body.set('usuarioid', "703021");
+    //body = body.set('password', "das");
+    return this.http
+      .post('http://157.245.227.216:3000/api/obtieneCoordenadasXUsuario', body, {
+        headers: myheader,
+      });
+      //.subscribe();
+
+
+      
   }
 }
